53033b40f0d8d48f5d6db3d334eb4934797dd564
Hugh Emberson <hugh.emberson@gmail.com>
2026-01-22 17:44:11 +1300

    Disable tabs and fullscreen on macOS main window

--- ShioriShare/ShioriShareApp.swift
+++ ShioriShare/ShioriShareApp.swift
@@ -1,10 +1,40 @@
 import SwiftUI
+#if os(macOS)
+import AppKit
+#endif
 
 @main
 struct ShioriShareApp: App {
+    #if os(macOS)
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    #endif
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        #if os(macOS)
+        .commands {
+            CommandGroup(replacing: .newItem) { }
+            CommandGroup(replacing: .toolbar) { }
+        }
+        #endif
     }
 }
+
+#if os(macOS)
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        NSWindow.allowsAutomaticWindowTabbing = false
+    }
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        DispatchQueue.main.async {
+            for window in NSApplication.shared.windows {
+                window.collectionBehavior.remove(.fullScreenPrimary)
+                window.collectionBehavior.insert(.fullScreenNone)
+            }
+        }
+    }
+}
+#endif
